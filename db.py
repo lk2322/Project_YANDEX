17fc03568ff64cffdcd2153ca9d7471d097a658c
lk2322 <lk2322@ya.ru>
2020-10-14 11:03:55 +0500

    init

---
+++ db.py
@@ -0,0 +1,91 @@
+from peewee import *
+import secrets
+import datetime
+
+db = SqliteDatabase('main.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    username = CharField(unique=True)
+    pwhash = CharField()
+
+
+class Thread(BaseModel):
+    pass
+
+
+class Participant(BaseModel):
+    thread_id = ForeignKeyField(Thread, backref='participant')
+    user_id = ForeignKeyField(User)
+
+
+class Message(BaseModel):
+    thread_id = ForeignKeyField(Thread, backref='messages')
+    user_id = ForeignKeyField(User)
+    text = CharField()
+    time = DateTimeField()
+
+
+def get_user(login):
+    db.connect(reuse_if_open=True)
+    try:
+        user = User.get(User.username == login)
+    except Exception:
+        return
+    return user
+    db.close()
+
+
+def create_thread(*usrnames):
+    db.connect(reuse_if_open=True)
+    thread = Thread.create()
+    for i in usrnames:
+        Participant.create(thread_id=thread, user_id=User.get(User.username == i))
+    db.close()
+
+
+def get_threads(user_id):
+    threads = Thread.select().join(Participant).where(Participant.user_id == user_id)
+    threads_dict = {}
+    for i in threads:
+        for j in i.participant:
+            if j.user_id.id == user_id:
+                continue
+            # {имя: id беседы}
+            threads_dict[j.user_id.username] = i.id
+    return threads_dict
+
+
+def get_messages(thread_id):
+    messages = Message.select().where(Message.thread_id == thread_id)
+    messages_dict = {}
+    for i in messages:
+        messages_dict[i.id] = {'datetime': i.time,
+                               'text': i.text,
+                               'user_id': i.user_id.id,
+                               'username': i.user_id.username}
+    return messages_dict
+
+
+def create_message(text, thread_id, from_id):
+    db.connect(reuse_if_open=True)
+    Message.create(thread_id=thread_id, text=text,
+                   user_id=from_id, time=datetime.datetime.now())
+    db.close()
+
+
+def create_user(login, pwhash):
+    db.connect(reuse_if_open=True)
+    User.create(username=login, pwhash=pwhash)
+    db.close()
+
+
+if __name__ == "__main__":
+    db.create_tables([User, Thread, Participant, Message])
+    a = get_threads(1)
+    print(a)
---
+++ server.py
@@ -0,0 +1,97 @@
+import flask
+import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.exceptions import HTTPException
+import jwt
+import datetime
+import os
+import time
+SECRET_KEY = os.getenv("SECRET_KEY")
+INVALID_TOKEN_ERROR = 'invalid token'
+app = flask.Flask(__name__)
+
+
+def exp_tommorow():
+    return datetime.datetime.now() + datetime.timedelta(days=1)
+
+
+def check_token(token: str):
+    try:
+        user = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        if user['exp'] < time.time():
+            return
+        else:
+            return user
+    except jwt.exceptions.DecodeError:
+        return
+
+
+@app.route('/login')
+def check_login():
+    args = flask.request.args
+    login = args['login']
+    passw = args['password']
+    user = db.get_user(login)
+    if check_password_hash(user.pwhash, passw):
+        return jwt.encode({'username': login, 'user_id': user.id, 'exp': exp_tommorow()}, SECRET_KEY, algorithm='HS256')
+
+
+@app.route('/register')
+def register():
+    args = flask.request.args
+    login = args['login']
+    passw = args['password']
+    if db.get_user(login):
+        return flask.abort(400, 'username_exists')
+    db.create_user(login, generate_password_hash(passw))
+    return 'ok'
+
+
+@app.route('/new_thread')
+def create_thread():
+    args = flask.request.args
+    token = args['token']
+    username = args['username']
+    user = check_token(token)
+    if not user:
+        return flask.abort(400, INVALID_TOKEN_ERROR)
+    db.create_thread(user['username'], username)
+    return 'ok'
+
+
+@app.route('/send_message')
+def send_message():
+    args = flask.request.args
+    token = args['token']
+    text = args['text']
+    thread_id = args['thread_id']
+    user = check_token(token)
+    if not user:
+        return flask.abort(400, INVALID_TOKEN_ERROR)
+    db.create_message(text, thread_id, user['user_id'])
+    return 'ok'
+
+
+@app.route('/get_threads')
+def get_thread():
+    args = flask.request.args
+    token = args['token']
+    user = check_token(token)
+    if not user:
+        return flask.abort(400, INVALID_TOKEN_ERROR)
+    return db.get_threads(user['user_id'])
+
+
+@app.route('/get_messages')
+def messages():
+    args = flask.request.args
+    token = args['token']
+    thread_id = args['thread_id']
+    user = check_token(token)
+    if not user:
+        return flask.abort(400, INVALID_TOKEN_ERROR)
+    return db.get_messages(thread_id)
+
+
+if __name__ == "__main__":
+    app.run()
